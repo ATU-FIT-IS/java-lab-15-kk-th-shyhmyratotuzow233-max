26c6d03fbadb64ccb2da9156da76f601803c580e
shyhmyratotuzow233-max <shyhmyratotuzow233@gmail.com>
2026-04-28 12:09:06 +0500

    Update main2.java

--- main2.java
+++ main2.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 1-ден N-ге дейінгі жұп сандардың қосындысы
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 2; i <= n; i += 2) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
