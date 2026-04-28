e1d9432bfb5006728e87d5c52cf009222bae7a51
shyhmyratotuzow233-max <shyhmyratotuzow233@gmail.com>
2026-04-28 11:40:38 +0500

    Update MainTest.java

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,15 +1,39 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
-public class MainTest {
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    @Test
-    void testSum() {
-        assertEquals(5, Main.sum(2, 3));
-    }
+        // 2. Екі санның үлкенін табу
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-    @Test
-    void testNegative() {
-        assertEquals(-1, Main.sum(2, -3));
+        if (a > b) {
+            System.out.println(a);
+        } else if (b > a) {
+            System.out.println(b);
+        } else {
+            System.out.println("Equal");
+        }
+
+        // 3. 1-ден N-ге дейінгі сандардың қосындысы
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+        // 4. Факториалды есептеу
+        int num = sc.nextInt();
+        long fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        System.out.println(fact);
+
+        sc.close();
     }
 }
